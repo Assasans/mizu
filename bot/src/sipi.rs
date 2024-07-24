1b3c820b335471db1989937a83b4c3605e900bae
Daniil Pryima <swimmin2@gmail.com>
2024-07-24 11:44:30 +0300

    runtime: Pass startup address to new cores

--- bot/src/sipi.rs
+++ bot/src/sipi.rs
@@ -18,13 +18,16 @@ pub struct SipiHandler {
 #[async_trait]
 impl InterruptHandler for SipiHandler {
   async fn handle(&self, cpu: &mut Cpu) {
-    debug!("creating new core...");
+    let pc = cpu.regs[10];
+    debug!("creating new core, pc={:#18x}...", pc);
+
     let isolate = cpu.isolate.as_ref().unwrap().upgrade().unwrap();
     let id = isolate.cores.lock().unwrap().len() as u16;
     let cpu = isolate.add_core(Cpu::new(id, isolate.bus.clone(), Some(Arc::downgrade(&isolate))));
 
     {
       let mut cpu = cpu.lock().await;
+      cpu.pc = pc;
       cpu.ivt.insert(11, Arc::new(Box::new(DumpPerformanceHandler { context: self.context.clone() })));
       cpu.ivt.insert(12, Arc::new(Box::new(HttpHandler { context: self.context.clone() })));
       cpu.ivt.insert(14, Arc::new(Box::new(LogHandler { context: self.context.clone() })));
@@ -33,6 +36,9 @@ impl InterruptHandler for SipiHandler {
       cpu.ivt.insert(17, Arc::new(Box::new(SipiHandler { context: self.context.clone() })));
     }
 
-    self.context.run_core(cpu).await.unwrap();
+    let context = self.context.clone();
+    tokio::spawn(async move {
+      context.run_core(cpu).await.unwrap();
+    });
   }
 }
--- temp/src/main.rs
+++ temp/src/main.rs
@@ -11,7 +11,26 @@ use prelude::*;
 #[link_section = ".start"]
 #[no_mangle]
 pub unsafe extern "C" fn _start() {
-  println!("Hello, 水の世界！");
+  asm!("csrrw zero, 0x320, t0", in("t0") 2);
+  let addr = _ap_startup as *const ();
+  println!("ap startup addr: {:?}", addr);
+  asm!("", in("a0") addr);
   syscall(17);
   halt();
+}
+
+pub unsafe extern "C" fn _ap_startup() {
+  asm!("csrrw zero, 0x320, t0", in("t0") 2);
+  println!("Hello, 水の世界！");
+  let fiba = fibonacci(25);
+  println!("{fiba}");
+  halt();
+}
+
+fn fibonacci(n: u32) -> u32 {
+  match n {
+    0 => 1,
+    1 => 1,
+    _ => fibonacci(n - 1) + fibonacci(n - 2),
+  }
 }
