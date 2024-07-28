adb5ffc1db12712b12ac1125b51bb13456f62cf1
Daniil Pryima <swimmin2@gmail.com>
2024-07-28 16:07:56 +0300

    hal: IVT declaration macro

--- hal-rs/hal/src/ivt.rs
+++ hal-rs/hal/src/ivt.rs
@@ -1,4 +1,4 @@
-use core::arch::asm;
+use core::arch::{asm, global_asm};
 use core::ffi::c_void;
 
 extern "C" {
@@ -17,4 +17,18 @@ pub unsafe fn __init_ivt_vector(address: *const c_void) {
 
 pub unsafe fn __wfi() {
   asm!("wfi");
+}
+
+#[macro_export]
+macro_rules! ivt {
+  ($($index:expr => $handler:ident),* $(,)?) => {
+    $(
+      global_asm!(
+        ".section .text.ivt",
+        concat!(".org .text.ivt + ", $index, " * 4"),
+        "jal {}",
+        sym $handler
+      );
+    )*
+  };
 }
--- temp/src/main.rs
+++ temp/src/main.rs
@@ -8,17 +8,39 @@ mod prelude;
 
 use prelude::*;
 
+use mizu_hal::discord::*;
+use mizu_hal::discord::discord::*;
+use mizu_hal::discord::discord::discord_ex_request::DiscordExRequestUnion;
+use mizu_hal::discord::prost::Message;
+use mizu_hal::discord::prost::alloc::string::ToString;
+
 #[link_section = ".start"]
 #[no_mangle]
 pub unsafe extern "C" fn _start() {
   __init_ivt_vector(core::ptr::addr_of!(__IVT_START));
-  __wfi();
-  println!("absolute={:?} relative={:?}", now_absolute(), Instant::now());
+  loop {
+    __wfi();
+  }
   halt();
 }
 
-global_asm!(r"
-.section .text.ivt
-.org .text.ivt + 17*4
-  mret
-");
+pub unsafe extern "C" fn int_discord() {
+  let len: u64;
+  let ptr: *const core::ffi::c_void;
+  asm!("", out("a0") len, out("a1") ptr);
+  println!("discord interrupt: len={}, ptr={:#x}", len, ptr as u64);
+  let data = core::slice::from_raw_parts(ptr as *const u8, len as usize);
+  // println!("{:?}", data);
+  let event = DiscordExEvent::decode(data).unwrap();
+  println!("{:?}", event);
+  asm!("mret");
+}
+
+pub unsafe extern "C" fn int_noop() {
+  asm!("mret");
+}
+
+ivt! {
+  1 => int_noop,
+  17 => int_discord
+}
