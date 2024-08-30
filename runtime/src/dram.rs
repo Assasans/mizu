199e1e29c1143012bcf3f276313432eea25708d5
Daniil Pryima <swimmin2@gmail.com>
2024-08-30 20:36:13 +0300

    runtime: Unroll DRAM code

--- runtime/src/dram.rs
+++ runtime/src/dram.rs
@@ -7,53 +7,42 @@ use crate::exception::Exception;
 
 pub struct Dram {
   pub dram: Vec<u8>,
-  pub code_range: Range<u64>,
 }
 
 impl Dram {
   pub fn new(code: Vec<u8>) -> Self {
     let mut dram = vec![0; DRAM_SIZE as usize];
     dram.splice(..code.len(), code);
-    Self {
-      dram,
-      code_range: 0..0,
-    }
+    Self { dram }
   }
 
-  // addr/size must be valid. Check in bus
   pub fn load(&self, addr: u64, size: u64) -> Result<u64, Exception> {
-    if ![8, 16, 32, 64].contains(&size) {
-      error!("unaligned load at 0x{addr:x}, {size}");
-      return Err(Exception::LoadAccessFault(addr));
-    }
-    let nbytes = size / 8;
     let index = addr as usize;
-    let mut code = self.dram[index] as u64;
-    // shift the bytes to build up the desired value
-    for i in 1..nbytes {
-      code |= (self.dram[index + i as usize] as u64) << (i * 8);
+    match size {
+      8 => Ok(self.dram[index] as u64),
+      16 => Ok(u16::from_le_bytes(self.dram[index..index + 2].try_into().unwrap()) as u64),
+      32 => Ok(u32::from_le_bytes(self.dram[index..index + 4].try_into().unwrap()) as u64),
+      64 => Ok(u64::from_le_bytes(self.dram[index..index + 8].try_into().unwrap())),
+      _ => {
+        error!("unaligned load at 0x{addr:x}, {size}");
+        Err(Exception::LoadAccessFault(addr))
+      }
     }
-
-    Ok(code)
   }
 
-  // addr/size must be valid. Check in bus
   pub fn store(&mut self, addr: u64, size: u64, value: u64) -> Result<(), Exception> {
-    if ![8, 16, 32, 64].contains(&size) {
-      return Err(Exception::StoreAMOAccessFault(addr));
-    }
-
-    if self.code_range.contains(&addr) {
-      warn!("tried to overwrite code segment at 0x{:x} with 0x{:x}", addr, value);
-      return Err(Exception::StoreAMOAccessFault(addr));
-    }
-
-    let nbytes = size / 8;
     let index = addr as usize;
-    for i in 0..nbytes {
-      let offset = 8 * i as usize;
-      self.dram[index + i as usize] = ((value >> offset) & 0xff) as u8;
+    match size {
+      8 => self.dram[index] = value as u8,
+      16 => self.dram[index..index + 2].copy_from_slice(&u16::to_le_bytes(value as u16)),
+      32 => self.dram[index..index + 4].copy_from_slice(&u32::to_le_bytes(value as u32)),
+      64 => self.dram[index..index + 8].copy_from_slice(&u64::to_le_bytes(value)),
+      _ => {
+        error!("unaligned store at 0x{addr:x}, {size}");
+        return Err(Exception::StoreAMOAccessFault(addr));
+      }
     }
+
     Ok(())
   }
 }
